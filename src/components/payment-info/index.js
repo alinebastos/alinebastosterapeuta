21a85bc6c044098ccf78cedb77801ae7bf1bf856
Aline Bastos <alineabp@gmail.com>
2022-08-26 11:16:21 -0300

    Change value

--- src/components/payment-info/index.js
+++ src/components/payment-info/index.js
@@ -14,11 +14,11 @@ function PaymentInfo () {
         <>
             <Icon icon={faEllipsis} />
             <p><b>Informações sobre o valor e as formas de pagamento:</b></p>
-            <p><b>Valor: </b>R$ 108,00</p>
+            <p><b>Valor: </b>R$ 144,00</p>
             <p><b>Formas de pagamento</b></p>
             <p><b>PIX:</b> alineabp@gmail.com</p>
             <p><b>PayPal:</b> alineabp@gmail.com</p>
-            <p><b>Cartão de Crédito (Parcelamento): </b><a href="https://mpago.la/1jQYQpk" target="_blank" rel="noreferrer" >Link para o Mercado Pago</a></p>
+            <p><b>Cartão de Crédito (Parcelamento): </b><a href="https://mpago.la/111hYP8" target="_blank" rel="noreferrer" >Link para o Mercado Pago</a></p>
             <p><b>TED/DOC:</b><br />
             Aline Bastos Pinto<br />
             Banco: Nubank - 0260<br />

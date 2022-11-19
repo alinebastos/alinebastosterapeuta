0ffe1e7519edb918739c80c0eee96a929fee325c
Aline Bastos <alineabp@gmail.com>
2022-11-19 02:56:55 -0300

    Add more Q&A

--- src/pages/faq-apometria-cosmica/index.js
+++ src/pages/faq-apometria-cosmica/index.js
@@ -25,7 +25,7 @@ const AccordionTitle = styled.div`
     font-weight: 600;
     cursor: pointer;
     color: #666;
-    padding: 0.5em 1.5em;
+    padding: 1em 1.5em;
     border: solid 1px #ccc;
     display: flex;
     justify-content: space-between;
@@ -86,7 +86,7 @@ function FaqApometriaCosmica() {
                 Perguntas e Respostas
             </h2>
             <Wrapper>
-                <h3>Material</h3>
+                <h3>Materiais</h3>
                 <Accordion title="Qual o melhor material para imprimir a interface?">
                     Existem várias opções, as mais resistentes e mais usadas são: em papel foto plastificado, em material de ímã e em PVC.<br />
                     Se for enviar para uma gráfica, prefira enviar por e-mail, pois o whatsapp diminui a qualidade do arquivo.
@@ -100,9 +100,58 @@ function FaqApometriaCosmica() {
                     Pode experimentar vários pêndulos até encontrar o que te escolheu.<br />
                     Sobre o material do pêndulo, não tem melhor ou pior, tem o que ressoa com você.
                 </Accordion>
+                <Accordion title="Quais cristais são indicados para colocar em cada chakra na interface?">
+                    Não tem necessidade de ser o cristal do chakra, pode ser o que você tem e sentir.<br />
+                    Em lojas de artigos esotéricos é muito comum achar kits prontos com as pedras dos 7 chakras.<br />
+                    Caso queira comprar separado, ou por curiosidade, aqui vão alguns exemplos:<br />
+                    1º Chakra: Jaspe vermelho, turmalina negra, ágata vermelha, granada ou obsidiana;<br />
+                    2º Chakra: Calcita laranja, ágata de fogo, jaspe madeira, cornalina e coral;<br />
+                    3º Chakra: Âmbar, ouro, calcita amarela, olho de tigre e topázio imperial;<br />
+                    4º Chakra: Turmalina melancia, quartzo verde, quartzo rosa, amazonita, jade, musgo e esmeralda;<br />
+                    5º Chakra: Turquesa, água marinha e quartzo azul;<br />
+                    6º Chakra: Lápis lazuli, safira azul e azurita;<br />
+                    7º Chakra: Angelita, ametista, quartzo branco e topázio incolor.
+                </Accordion>
                 <h3>Curso</h3>
                 <Accordion title="O curso ficará gravado?">
                     Sim, o curso ficará gravado e disponibilizado na plataforma Hotmart.
+                </Accordion>
+                <h3>Apometria</h3>
+                <Accordion title="Em que momento utilizaremos o comando ventor solar? Seria quando a mesa apontar para energia telurica ou bases astrais?">
+                    Sim, nesses momentos.
+                </Accordion>
+                <Accordion title="A Apometria pode ser feita para pets?">
+                    Sim, pode.
+                </Accordion>
+                <Accordion title="Quando mostra que precisa de cromoterapia, devemos fazer o comando?">
+                    Sim, pode fazer o comando.
+                </Accordion>
+                <Accordion title="Pode acontecer de não precisar ir para o hospital?">
+                    É raro, mas pode acontecer.
+                </Accordion>
+                <Accordion title="Sobre implantes negativos, podem estar atuando nos chakras e nos corpos sutis?">
+                    Sim, pode ter implantes tanto chakras quantos nos corpos sutis.
+                </Accordion>
+                <Accordion title="Existe implante no corpo físico?">
+                    Existe, mas não é tão comum. A apometria trata especificamente dos implantes negativos nos corpos sutis, mesmo quando falamos em locais específicos, também é a nível energético.
+                </Accordion>
+                <Accordion title="Qual a melhor forma de fazer o retorno do atendimento?">
+                    Existem algumas formas:<br />
+                    Você pode ir anotando tudo na ficha de atendimento e depois relatar esses resultados em um áudio.<br />
+                    Você pode fazer diversos áudios curtos, com um tema por áudio, por exemplo: Áudio com as verificações do Hospital do astral, outro áudio com as verificações dos contratos, etc.
+                    Você também pode ir gravando o áudio durante o atendimento, pausando enquanto estiver fazendo as verificações e descrevendo no audio somente o resultado de cada verificação.
+                </Accordion>
+                <Accordion title="Para complementar as informações posso usar outras técnicas que já utilizo, como radiestesia ou registros akáshicos?">
+                    Pode sim, você pode integrar outras técnicas com a apometria, por isso  o "Dinâmica" no nome.
+                </Accordion>
+                <Accordion title="Quando aparecer um obsessor encarnado ou desencarnado, e mostrar que não pode ser encaminhado, como proceder?">
+                    Sugerimos que você utilize o comando de harmonização. Tem casos em que não é permitido encaminhar pois o assistido está muito conectado com o obsessor, fazer essa ruptura pode prejudicar o assistido.
+                </Accordion>
+                <Accordion title="É normal ficar mais disposta, com mais energia, quando a gente faz a apometria para outras pessoas?">
+                    Sim, é normal, pois você vai recebendo junto "coisas" necessárias a você.
+                </Accordion>
+                <Accordion title="Quando a gente faz pra gente, dizemos nosso nome ou falamos 'para mim'? Faz diferença?">
+                    Pode falar como se sentir melhor.
                 </Accordion>
             </Wrapper>
         </>

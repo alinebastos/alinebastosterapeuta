8aa4619de767697c492fb8572baf83549dfb20b7
Aline Bastos <alineabp@gmail.com>
2023-01-03 13:12:27 -0300

    Publish faq apometria

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer'
 import AuraMaster from './pages/aura-master'
-/* import ApometriaCosmica from './pages/apometria-cosmica';
 import FaqApometriaCosmica from './pages/faq-apometria-cosmica';
+/* import ApometriaCosmica from './pages/apometria-cosmica';
 import Home from './pages/home';
 import CuraArcturiana from './pages/cura-arcturiana';
 import CursoCuraArcturiana from './pages/curso-cura-arcturiana';
@@ -16,8 +16,8 @@ function App() {
         <Header />
         <Routes>
           <Route exact path='/' element={<AuraMaster />} />
-          {/* <Route path='/apometria-cosmica' element={<ApometriaCosmica />} />
           <Route path='/faq-apometria-cosmica' element={<FaqApometriaCosmica />} />
+          {/* <Route path='/apometria-cosmica' element={<ApometriaCosmica />} />
           <Route path='/aura-master' element={<AuraMaster />} />          
           <Route path='/cura-arcturiana' element={<CuraArcturiana />} />
           <Route path='/curso-cura-arcturiana' element={<CursoCuraArcturiana />} options={{Header: () => null}} />
